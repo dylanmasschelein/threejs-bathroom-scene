38c9ab9e4e773d72a272d38e1dcb703183476951
dylanmasschelein <dylanmasschelein@gmail.comclear>
2021-08-04 13:44:28 -0700

    Add Walls and initial vanity to scene

--- src/App.js
+++ src/App.js
@@ -19,6 +19,17 @@ const App = () => {
         <Box />
         <Suspense fallback={null}>
           <Floor />
+          <Wall position={[0, 5, -5]} rotation={[0, 0, 0]} args={[15, 10]} />
+          <Wall
+            position={[-7.5, 5, 0]}
+            rotation={[0, -Math.PI / -2, 0]}
+            args={[10, 10]}
+          />
+          <Wall
+            position={[7.5, 5, 0]}
+            rotation={[0, -Math.PI / 2, 0]}
+            args={[10, 10]}
+          />
         </Suspense>
       </Canvas>
     </div>
--- src/Box.js
+++ src/Box.js
@@ -1,19 +1,8 @@
-// import { useBox } from "@react-three/cannon";
-
 const Box = (props) => {
-  //   const meshRef = useRef();
-  //   const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0], ...props }));
-  //   useFrame(() => (meshRef.current.rotation.z += 0.01));
   return (
-    <mesh
-      //   onClick={() => {
-      //     api.velocity.set(0, 2, 0);
-      //   }}
-      //   ref={ref}
-      position={[0, 0.5, 0]}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial attach='material' color='blue' />
+    <mesh position={[-5.5, 2, -3.75]}>
+      <boxGeometry args={[4, 4, 2.5]} />
+      <meshStandardMaterial attach='material' color='white' />
     </mesh>
   );
 };
